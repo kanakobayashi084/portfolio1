dea2ba89b8ef8d8a8aeeb5cf4bd0e884886c0607
k-kobayashi <k.kobayashiii084@gmail.com>
2024-03-18 23:50:00 +0900

    3月18日⓶

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const swiper = new Swiper(".swiper", {
-        loop: true,
-        slidesPerView: 3,
-        speed: 6000,
-        allowTouchMove: false,
-        autoplay: {
-            delay: 0,
-        }
-    });
+const swiper = new Swiper('.swiper',{
+    loop: true,
+    slidesPerView: 2,
+    speed: 6000,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 0,
+    }
+});
